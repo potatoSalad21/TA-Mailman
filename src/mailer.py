be510bfc158fcdfb4084c072f0023ef413a22226
potatoSalad21 <datuna.ziraqishvili@gmail.com>
2025-11-22 01:52:57 +0400

    Feat: class wrapper for all mail functionality

--- src/mailer.py
+++ src/mailer.py
@@ -1,61 +1,70 @@
 import os.path
+import json
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+from email.message import EmailMessage
 
 SCOPES = ["https://mail.google.com/"]
-RESULTS_PATH = "../data/grades.json"
+UNI_MAIL_PREF = "@freeuni.edu.ge"
+
+TA_MAIL = "dzira24@freeuni.edu.ge"
+SUBJECT = "Programming Methodology Assignment"
 
 
-def auth():
-    creds = None
-    if os.path.exists("token.json"):
-        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+class GradeMailer:
+    _creds = None
+    _grades = None
 
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                "credentials.json", SCOPES
-            )
-            creds = flow.run_local_server(port=0)
+    def __init__(self):
+        _creds = self.auth()
 
-        with open("token.json", "w") as token:
-            token.write(creds.to_json())
+    def readGrades(self, path):
+        with open(path, 'r',) as data:
+            self._grades = json.load(data)
 
-    return creds
+    def mailGrades(self):
+        for prefix in self._grades:
+            try:
+                service = build("gmail", "v1", credentials=self._creds)
+                msg = EmailMessage()
+                msg["To"] = prefix + UNI_MAIL_PREF
+                msg["From"] = TA_MAIL
+                msg["Subject"] = SUBJECT
+                msg.set_content(self._grades[prefix])
 
+                encoded_msg = base64.urlsafe_b64encode(msg.as_bytes()).decode()
+                send_msg = (
+                    service.users()
+                        .messages(),
+                        .send(userId="me", body=create_message)
+                        .execute()
+                )
 
-def readResults(path):
-    pass
+                print(send_msg)
 
+            except HttpError as err:
+                print(f"Http error: {err}")
+                send_msg = None
 
-def sendResults():
-    pass
+    def auth(self):
+        creds = None
+        if os.path.exists("token.json"):
+            creds = Credentials.from_authorized_user_file("token.json", SCOPES)
 
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    "credentials.json", SCOPES
+                )
+                creds = flow.run_local_server(port=0)
 
-def main():
-    creds = auth()
+            with open("token.json", "w") as token:
+                token.write(creds.to_json())
 
-    try:
-        service = build("gmail", "v1", credentials=creds)
-        results = service.users().labels().list(userId="me").execute()
-        labels = results.get("labels", [])
-
-        if not labels:
-            print("No labels found.")
-            return
-        print("Labels:")
-        for label in labels:
-            print(label["name"])
-
-    except HttpError as error:
-        print(f"An error occurred: {error}")
-
-
-if __name__ == "__main__":
-    main()
+        return creds
